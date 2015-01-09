6acef929df6c5a5c3a2aba6fcaf94c8507530fd0
bv-globe <benjamin.valentine@globe.com>
2015-01-09 12:01:52 -0500

    DM-175: Search Experience
    Remove console.log (55), fixed bug with undefined DOM elements, added
    search2 instance in index.html script.

--- dist/js/assets/searchPanel.js
+++ dist/js/assets/searchPanel.js
@@ -27,16 +27,18 @@ dm.select2 = {};
 
 		this.inputs = {
 			$main: $('.page-search__dropdown--main'),
-			$filters: $('.page-search__dropdown--filter, .page-search__dropdown--filter-wide, .page-search__dropdown--filter-advanced'),
+			$filters: $('.page-search__dropdown--filter, 
+						.page-search__dropdown--filter-wide, 
+						.page-search__dropdown--filter-advanced'),
 			$tokenize: $('.page-search__input--tokenize')
 		}
 
 		this.btns = {
-			$lvl2t: $('.page-search__buttons--level-two-toggle')[0],
-			$lvl3t: $('.page-search__buttons--level-three-toggle'),
-			$close: $('.page-search__buttons--close'),
-			$svSearch: $('.page-search__buttons--save-search'), 
-			$search: $('.page-search__buttons--search')
+			$lvl2t: $('.page-search__button--level-two-toggle'),
+			$lvl3t: $('.page-search__button--level-three-toggle'),
+			$close: $('.page-search__button--close'),
+			$svSearch: $('.page-search__button--save-search'), 
+			$search: $('.page-search__buttons--submit')
 		}
 
 		this.init();	
@@ -52,7 +54,6 @@ dm.select2 = {};
 			this.lvls.$two.hide();
 			this.lvls.$three.hide();
 
-			console.log(this.btns.$lvl2t);
 
 			dm.selectize(this.inputs.$main, {persist: false});
 			dm.selectize(this.inputs.$tokenize, {
@@ -78,18 +79,28 @@ dm.select2 = {};
 
 			self.btns.$lvl2t.on('click', function(e){
 				e.preventDefault();
+				var device = $(this).hasClass('mobile') ? 'mobile' : 'desktop';
 				var arrow = $(this).children('span');
-				if(self.allOpen){
-					self.lvls.$three.hide();
-					self.lvls.$two.hide();
-					self.btns.$lvl3t.removeClass('.select2-panel-open');
-					arrow.toggle();
-					self.allOpen = false;
-				}else{
-					self.lvls.$two.toggle();
-					arrow.toggle();
-					self.allOpen = false;
+				if(device == 'desktop'){
+					if(self.allOpen){
+						arrow.toggle();
+						self.lvls.$three.hide();
+						self.lvls.$two.hide();
+						self.btns.$lvl3t.removeClass('.select2-panel-open');
+						self.allOpen = false;
+					}else{
+						arrow.toggle();
+						self.lvls.$two.toggle();
+						self.allOpen = false;
+					}
 				}
+
+				if(device == 'mobile'){
+					var lvls = $.merge(self.lvls.$two, self.lvls.$three);
+					arrow.toggle();
+					lvls.toggle();
+				}
+				
 			});
 
 			self.btns.$lvl3t.on('click', function(e){
