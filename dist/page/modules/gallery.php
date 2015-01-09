1bfa450ebb0f620ce39193b359bd8139eb82a7fc
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-09 14:12:36 -0500

    New Gallery Markup

--- dist/page/modules/gallery.php
+++ dist/page/modules/gallery.php
@@ -1,5 +1,29 @@
 <!-- Gallery Widget -->
 <div class="gallery-widget">
-    <img src="images/gallery/Gallery_06.jpg" />
+    
+    <div class="gallery-widget__main-wrapper">
+	    <div class="galley-widget__main-wrapper__scrolling-wrapper">
+	    	<img src="images/gallery/Gallery_06.jpg" />
+	    </div>
+
+	    <div class="gallery-widget__details">
+	    	<div class="gallery-widget__details__info-nav">
+	    		<a href="javascript:;">Show Captions</a>
+
+	    		<div class="counter">
+	    			<span class="current">1</span> of <span class="total">18</span>
+	    		</div>
+	    	</div>
+
+	    	<div class="caption">
+				<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
+				Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
+				on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
+				million price tag).</p>
+
+				<p class="photo-credit">Photo Credit- Elyssa Cohen</p>
+	    	</div>
+	    </div>
+    </div>
 </div>
 <!-- Gallery Widget -->
