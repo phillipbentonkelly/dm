c894200827c257b59cca6bef4d0ef9d72c1fbacf
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-12 15:41:30 -0500

    Fixing merge conflict in files ...

--- js/assets/responsive-gallery.js
+++ js/assets/responsive-gallery.js
@@ -6,7 +6,6 @@ if (typeof bdc === 'undefined') { bdc = {}; }
 (function( win, $, undefined ) {
 	'use strict';
 
-<<<<<<< HEAD
 	var info = {};
 	var module = {};
 	var _engine = {};
@@ -71,8 +70,5 @@ if (typeof bdc === 'undefined') { bdc = {}; }
 
     	console.log(info);
     };
-
-=======
->>>>>>> 21fcc25b3e7b6438f9322dc5450988c40169f712
 
 })(window, jQuery);
