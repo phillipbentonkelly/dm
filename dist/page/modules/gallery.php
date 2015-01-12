e005a7f422fca9d1ca8594f3ed6a1686d631106c
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-12 13:20:40 -0500

    Updated the markup for the gallery widget and added the .csslintrc config file

--- dist/page/modules/gallery.php
+++ dist/page/modules/gallery.php
@@ -6,7 +6,7 @@
 
 	    <div class="gallery-widget__main-wrapper__scrolling-wrapper">
 	    	<div class="slide">
-	    		<img src="images/gallery/Gallery_01.jpg" />
+	    		<img src="images/gallery/Gallery_01.jpg" srcset="images/gallery/Gallery_01.jpg 618w" alt="Gallery 1" />
 		    	<div class="hidden-caption">
 		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
 					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
@@ -16,49 +16,6 @@
 		    	</div>
 	    	</div>
 
-	    	<div class="slide">
-	    		<img src="images/gallery/Gallery_02.jpg" />
-		    	<div class="hidden-caption">
-		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
-					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
-					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
-					million price tag).</p>
-					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
-		    	</div>
-	    	</div>
-	    	
-	    	<div class="slide">
-	    		<img src="images/gallery/Gallery_03.jpg" />
-		    	<div class="hidden-caption">
-		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
-					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
-					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
-					million price tag).</p>
-					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
-		    	</div>
-	    	</div>
-	    	
-	    	<div class="slide">
-	    		<img src="images/gallery/Gallery_04.jpg" />
-		    	<div class="hidden-caption">
-		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
-					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
-					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
-					million price tag).</p>
-					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
-		    	</div>
-	    	</div>
-	    	
-	    	<div class="slide">
-	    		<img src="images/gallery/Gallery_05.jpg" />
-		    	<div class="hidden-caption">
-		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
-					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
-					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
-					million price tag).</p>
-					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
-		    	</div>
-	    	</div>
 	    </div>
 	    <div class="gallery-widget__details">
 	    	<div class="gallery-widget__details__info-nav">
