bfa4fae6eeb1ada7a6a419e1e8e4979b7d594190
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-12 03:23:02 -0500

    Finished the markup for the gallery, now will need to focus on the functionality.

--- dist/page/modules/gallery.php
+++ dist/page/modules/gallery.php
@@ -1,21 +1,70 @@
 <!-- Gallery Widget -->
 <div class="gallery-widget">
     <div class="gallery-widget__main-wrapper">
-	    <div class="galley-widget__main-wrapper__scrolling-wrapper">
-	    	<img src="images/gallery/Gallery_06.jpg" />
-	    	<div class="hidden-caption">
-	    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
-				Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
-				on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
-				million price tag).</p>
-				<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
+    	<a href="javascript:;" class="gallery-widget__main-wrapper__nav-arrows chevron left"></a>
+    	<a href="javascript:;" class="gallery-widget__main-wrapper__nav-arrows chevron right"></a>
+
+	    <div class="gallery-widget__main-wrapper__scrolling-wrapper">
+	    	<div class="slide">
+	    		<img src="images/gallery/Gallery_01.jpg" />
+		    	<div class="hidden-caption">
+		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
+					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
+					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
+					million price tag).</p>
+					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
+		    	</div>
+	    	</div>
+
+	    	<div class="slide">
+	    		<img src="images/gallery/Gallery_02.jpg" />
+		    	<div class="hidden-caption">
+		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
+					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
+					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
+					million price tag).</p>
+					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
+		    	</div>
+	    	</div>
+	    	
+	    	<div class="slide">
+	    		<img src="images/gallery/Gallery_03.jpg" />
+		    	<div class="hidden-caption">
+		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
+					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
+					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
+					million price tag).</p>
+					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
+		    	</div>
+	    	</div>
+	    	
+	    	<div class="slide">
+	    		<img src="images/gallery/Gallery_04.jpg" />
+		    	<div class="hidden-caption">
+		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
+					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
+					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
+					million price tag).</p>
+					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
+		    	</div>
+	    	</div>
+	    	
+	    	<div class="slide">
+	    		<img src="images/gallery/Gallery_05.jpg" />
+		    	<div class="hidden-caption">
+		    		<p>Behold, it’s the totally baller house of the week: there’s a bar, a theater, an infinity pool, iPads.
+					Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
+					on the market for about three years but couldn’t entice a buyer (maybe because of its $13.9
+					million price tag).</p>
+					<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
+		    	</div>
 	    	</div>
 	    </div>
 	    <div class="gallery-widget__details">
 	    	<div class="gallery-widget__details__info-nav">
-	    		<a href="javascript:;" class="gallery-widget__details__info-nav__show-caption">Show Captions</a>
+	    		<a href="javascript:;" class="gallery-widget__details__info-nav__show-caption chevron bottom">Show Captions</a>
 	    		<div class="gallery-widget__details__info-nav__counter">
-	    			<span class="gallery-widget__details__info-nav__counter__current">1</span> of <span class="gallery-widget__details__info-nav__counter__total">18</span>
+	    			<span class="gallery-widget__details__info-nav__counter__current">1</span> of <span class="gallery-widget__details__info-nav__counter__total">5</span>
 	    		</div>
 	    	</div>
 	    	<div class="gallery-widget__details__caption">
