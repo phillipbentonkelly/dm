b6423ec5e5bd794bd40c7bcfabd1b2122b22154e
EthanGould <ethan.g.907@gmail.com>
2015-01-14 11:03:09 -0500

    added check box for serp right col map

--- dist/page-content/modules/search-map.php
+++ dist/page-content/modules/search-map.php
@@ -1,5 +1,5 @@
 <div class='map-header' style='text-align:right;margin:20px 0 5px 0;padding:15px;background:lightgrey;'>
-	<p>Redo search on move</p>
+	<p><span><input type='checkbox'/></span>Redo search on move</p>
 </div>
 
 <div class='map-placeholder' style='height:300px;width:100%;border: 1px solid black;box-sizing:border-box;'></div>
