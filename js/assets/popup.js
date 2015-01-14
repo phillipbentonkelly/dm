23d75ec8a01afd7b9939cb6ec542d1cbc118191c
EthanGould <ethan.g.907@gmail.com>
2015-01-14 17:19:17 -0500

    fixes to reltaed articles popup on serp

--- js/assets/popup.js
+++ js/assets/popup.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 
 	$('#mobile-articles-btn').click(function() {
 		var closeModal = $("<a style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
+		$('.related-articles').addClass('related-articles-popup');
 		$('.related-articles__section-header').append(closeModal);
-
 		$('.related-articles-modal').modal();
 	});
  
