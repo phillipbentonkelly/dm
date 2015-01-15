ec4d8b6292db893cc0417c7a6535bbd17b0ca585
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-15 14:38:03 -0500

    Added the comments engine to the Articles page.

--- dist/frameset.php
+++ dist/frameset.php
@@ -174,6 +174,9 @@
 
 				<!-- Related Articles -->
 				<?php include 'page-content/modules/related-articles.php'; ?>
+
+				<!-- Discus -->
+				<?php include 'page-content/modules/comments.php'; ?>
 			<?php } ?>
 			
 
@@ -320,5 +323,21 @@
 
 	<!-- Page Footer -->
     <?php include 'page/page-footer.php'; ?>
+
+    <!-- ARTICLES: Discus -->
+	<?php if ($pageType == 'articles') { ?>
+		<script type="text/javascript"> 
+			/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */ 
+			var disqus_shortname = 'bcomrealestate'; // required: replace example with your forum shortname 
+
+			/* * * DON'T EDIT BELOW THIS LINE * * */ 
+			(function () { 
+				var s = document.createElement('script'); s.async = true; 
+				s.type = 'text/javascript'; 
+				s.src = '//' + disqus_shortname + '.disqus.com/count.js'; 
+				(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s); 
+			}()); 
+		</script> 
+	<?php } ?>
 </body>
 </html>
