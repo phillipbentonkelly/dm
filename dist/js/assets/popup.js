f6d50945437d54b998b2f4a6a9512a1f7f11cf74
EthanGould <ethan.g.907@gmail.com>
2015-01-15 15:47:54 -0500

    moved js into ercternalfile for right rail map stickyness

--- dist/js/assets/popup.js
+++ dist/js/assets/popup.js
@@ -24,4 +24,22 @@ $(document).ready(function() {
 	$(document).on('click', '.close-modal', function() {
         $.modal.close();
     });
+
+
+    var $sidebar   = $(".sticky-map-wrapper"), 
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = 100;
+        
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+	});
 });
--- dist/page-content/modules/search-map.php
+++ dist/page-content/modules/search-map.php
@@ -1,26 +1,4 @@
-<script type="text/javascript">
-	$(function() {
 
-	    var $sidebar   = $(".sticky-map-wrapper"), 
-	        $window    = $(window),
-	        offset     = $sidebar.offset(),
-	        topPadding = 100;
-
-	    $window.scroll(function() {
-	    	if ($window.scrollTop() > 3200) return;
-	        if ($window.scrollTop() > offset.top) {
-	            $sidebar.stop().animate({
-	                marginTop: $window.scrollTop() - offset.top + topPadding
-	            });
-	        } else {
-	            $sidebar.stop().animate({
-	                marginTop: 0
-	            });
-	        }
-	    });
-	});
-	
-</script>
 
 <div class='sticky-map-wrapper'>
 	<div class='map-header' style='text-align:right;margin:95px 0 5px 0;padding:15px;background:lightgrey;'>
