227cc5d936a019addef3e0d6e4db357fb5accb61
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-21 11:43:04 -0500

    Updated the article social icons with sharethis plugin.

--- dist/page-section/social-icons.php
+++ dist/page-section/social-icons.php
@@ -1,11 +1,16 @@
 <!-- Social Icons -->
-<div class="social-icons">
-    <a href="javascript:;" class="social-icons__facebook">facebook</a>
+<div class="social-icons third-party-social-icons">
+	<span class='st_facebook_large' displayText='Facebook'></span> 
+	<span class='st_pinterest_large' displayText='Pinterest'></span> 
+	<span class='st_email_large' displayText='Email'></span> 
+	<span class='st_twitter_large' displayText='Tweet'></span> 
+	
+    <!--<a href="javascript:;" class="social-icons__facebook">facebook</a>
     <a href="javascript:;" class="social-icons__twitter">twitter</a>
     <a href="javascript:;" class="social-icons__share">share</a>
     <a href="javascript:;" class="social-icons__email">email</a>
     <a href="javascript:;" class="social-icons__comments">comment</a>
     <a href="javascript:;" class="social-icons__print">print</a>
-    <a href="javascript:;" class="social-icons__favorite">favorite</a>
+    <a href="javascript:;" class="social-icons__favorite">favorite</a>-->
 </div>
 <!-- Social Icons -->
