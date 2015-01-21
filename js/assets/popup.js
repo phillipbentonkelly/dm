1fa9d6ae745973d84b89d06dba4cc226fc9d3a9a
Chris George-O'Neil <chris.george-o'neil@globe.com>
2015-01-21 11:23:32 -0500

    Updated GH-Pages with the latest version of this repo that can be used for QA.

--- js/assets/popup.js
+++ js/assets/popup.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
 	});
 
 	$('#mobile-articles-btn').click(function() {
-		var closeModal = $("<a style='float:right' href='javascript:;'><img class='close-modal close-modal-js' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
+		var closeModal = $("<a style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
 		$('.related-articles').addClass('related-articles-popup');
 		$('.related-articles__section-header').append(closeModal);
 		$('.related-articles-modal').modal();
 	});
  
-	$('.mobile-contact-btn').click(function() {
-		var closeModal = $("<a style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal close-modal-js' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
+	$('#mobile-contact-btn').click(function() {
+		var closeModal = $("<a style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
 		$('.contact__header').append(closeModal);
 		$('.contact-agent-modal').modal();
 	});
@@ -27,6 +27,5 @@ $(document).ready(function() {
 
 	$(document).on('click', '.close-modal', function() {
         $.modal.close();
-        $('.close-modal-js').remove();
     });
 });
