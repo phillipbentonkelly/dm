b1697ca0c20a331d5387b84d5199ca0f6dc5c63d
EthanGould <ethan.g.907@gmail.com>
2015-01-21 17:04:50 -0500

    updates to styling on mobile share poup DM-159

--- dist/js/assets/popup.js
+++ dist/js/assets/popup.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 		$('.related-articles-modal').modal();
 	});
  
-	$('#mobile-contact-btn').click(function() {
+	$('.mobile-contact-btn').click(function() {
 		var closeModal = $("<a style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
 		$('.contact__header').append(closeModal);
 		$('.contact-agent-modal').modal();
--- dist/page-content/modules/share-modal.php
+++ dist/page-content/modules/share-modal.php
@@ -5,15 +5,17 @@
         <p>Start Sharing</p>
     </div>
     <div class="content">
-    	<div class='share-icons--row'>
-        	<span class='st_facebook_large' displayText='Facebook'></span> 
-        	<span class='st_pinterest_large' displayText='Pinterest'></span>
-	    </div>
+    	<div class='share-icons__wrapper'>
+	    	<div class='share-icons icons--right-col'>
+	        	<span class='st_facebook_large' displayText='Facebook'></span> 
+	        	<span class='st_pinterest_large' displayText='Pinterest'></span>
+		    </div>
 
-    	<div class='share-icons--row'>
-			<span class='st_email_large' displayText='Email'></span> 
-        	<span class='st_twitter_large' displayText='Tweet'></span>
-	    </div>
+	    	<div class='share-icons icons--left-col'>
+				<span class='st_email_large' displayText='Email'></span> 
+	        	<span class='st_twitter_large' displayText='Tweet'></span>
+		    </div>
+		</div>
     </div>
 </div>
 <!-- /MODAL: /SHARE PROPERTY -->
