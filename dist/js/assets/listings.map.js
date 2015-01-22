ce5750cf7b6ef7660f3c4956885337b73ab5cbeb
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-22 18:00:54 -0500

    Updated the Listing Map on the Neighborhood page to change the selected nav when you click one of the links.

--- dist/js/assets/listings.map.js
+++ dist/js/assets/listings.map.js
@@ -5,24 +5,36 @@ var listingMapObj = {};
     'use strict';
 
     var listingmap = {};
-    var _engine = {};
+    var _data = {};
 
     listingMap = function(){
     	if( ! (this instanceof listingMap))
     		return new listingMap();
-    }
+    };
 
     listingMap.prototype = {
     	init: function(){
     		listingmap.$links = $('.listings-map__navigation a');
+    		_data.currentNav = 'show-me';
+
+    		this.eventHandlers();
     	},
     	eventHandlers: function(e){
+    		listingmap.$links.on('click', function(){
+    			var thisObj = $(this);
+    			var newNav = thisObj.attr('data-map-item');
 
+    			if(_data.currentNav !== 'show-me' || _data.currentNav !== newNav){
+    				listingmap.$links.removeClass('selected');
+
+    				thisObj.addClass('selected');
+    			}
+    		});
     	}
-    }
-
+    };
 })(window, jQuery);
 
 $(function(){
-
+	listingMapObj = listingMap();
+	listingMapObj.init();
 });
