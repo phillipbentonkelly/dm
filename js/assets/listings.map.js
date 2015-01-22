62733cef903062093ec50253162925c567baae23
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-22 14:55:10 -0500

    Adding logic to toggle map navigation areas.

---
+++ js/assets/listings.map.js
@@ -0,0 +1,14 @@
+var listingMap = {};
+var listingMapObj = {};
+
+(function( win, $, undefined ) {
+    'use strict';
+
+    var listingmap = {};
+    var _engine = {};
+
+})(window, jQuery);
+
+$(function(){
+
+});
