cdd7fc87564d08d273d401f32f1570eac5fb9caa
EthanGould <ethan.g.907@gmail.com>
2015-01-22 10:42:26 -0500

    Updated GH-Pages with the latest version of this repo that can be used for QA.

--- js/assets/popup.js
+++ js/assets/popup.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	$('#mobile-save-btn').click(function() {
+	$('#mobile-save-btn, .mobile-save-btn').click(function() {
 		$('.save-search-modal').modal();
 	});
 
@@ -12,12 +12,12 @@ $(document).ready(function() {
 	});
  
 	$('.mobile-contact-btn').click(function() {
-		var closeModal = $("<a style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
+		var closeModal = $("<a class='close-modal-wrap' style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
 		$('.contact__header').append(closeModal);
 		$('.contact-agent-modal').modal();
 	});
 
-	$('#mobile-share-btn').click(function() {
+	$('#mobile-share-btn, .mobile-share-btn').click(function() {
 		$('.share-modal').modal();
 	});
 	
@@ -27,6 +27,7 @@ $(document).ready(function() {
 
 	$(document).on('click', '.close-modal', function() {
         $.modal.close();
+        $('body').find('.close-modal-wrap').remove();
     });
 
     // JS for SERP map/list toggle
