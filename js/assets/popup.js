7bced76aaa747c78bc03082d03a4f671271c70ee
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-22 14:57:21 -0500

    Updated GH-Pages with the latest version of this repo that can be used for QA.

--- js/assets/popup.js
+++ js/assets/popup.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	$('#mobile-save-btn, .mobile-save-btn').click(function() {
+	$('#mobile-save-btn').click(function() {
 		$('.save-search-modal').modal();
 	});
 
@@ -11,13 +11,13 @@ $(document).ready(function() {
 		$('.related-articles-modal').modal();
 	});
  
-	$('.mobile-contact-btn').click(function() {
-		var closeModal = $("<a class='close-modal-wrap' style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
+	$('#mobile-contact-btn').click(function() {
+		var closeModal = $("<a style='position:absolute;right:10px;top:10px;' href='javascript:;'><img class='close-modal' src='images/listings/x-icon.jpg' style='height:20px;width:20px;'></a>");
 		$('.contact__header').append(closeModal);
 		$('.contact-agent-modal').modal();
 	});
 
-	$('#mobile-share-btn, .mobile-share-btn').click(function() {
+	$('#mobile-share-btn').click(function() {
 		$('.share-modal').modal();
 	});
 	
@@ -27,29 +27,5 @@ $(document).ready(function() {
 
 	$(document).on('click', '.close-modal', function() {
         $.modal.close();
-        $('body').find('.close-modal-wrap').remove();
     });
-
-    // JS for SERP map/list toggle
-
-    $('.search-results__toggle-view').click(function() {
-		$('.serp-map-btn').toggleClass('serp-list-btn');
-		$('.serp-map--mobile-image').slideToggle(200);
-
-		if(!$('.serp-map-btn').hasClass('serp-list-btn')) {
-			$(this).children('p').text('Map');
-			$('.search-results__item').show();
-		} else {
-			$(this).children('p').text('List');
-			$('.search-results__item').not('#result2').hide();
-		}
-    });
-
-    // JS for serp header hover
-
-    $('.search-results__item').children('.header').on('touchstart touchend', function(e) {
-		e.preventDefault();
-		$(this).toggleClass('serp-item-hovered');
-	});
-
 });
