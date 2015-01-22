19a2a3d32678b21a91c30387504256c620574262
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-22 16:41:24 -0500

    More progress on the JS for the listing map navigation.

--- js/assets/listings.map.js
+++ js/assets/listings.map.js
@@ -7,6 +7,20 @@ var listingMapObj = {};
     var listingmap = {};
     var _engine = {};
 
+    listingMap = function(){
+    	if( ! (this instanceof listingMap))
+    		return new listingMap();
+    }
+
+    listingMap.prototype = {
+    	init: function(){
+    		listingmap.$links = $('.listings-map__navigation a');
+    	},
+    	eventHandlers: function(e){
+
+    	}
+    }
+
 })(window, jQuery);
 
 $(function(){
