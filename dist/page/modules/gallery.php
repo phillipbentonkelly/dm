d6f88ff834546e7f35a210a2451c901109dd2e32
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-01-28 05:08:33 -0500

    Updated the default caption text.

--- dist/page/modules/gallery.php
+++ dist/page/modules/gallery.php
@@ -38,10 +38,7 @@
 	    		</div>
 	    	</div>
 	    	<div class="gallery-widget__details__caption">
-				<p>Behold, it's the totally baller house of the week: there's a bar, a theater, an infinity pool, iPads.
-				Yes, there are iPads. This 1.32-acre, 10,279-square foot beachfront property in Cape Cod has been
-				on the market for about three years but couldn't entice a buyer (maybe because of its $13.9
-				million price tag).</p>
+				<p>Caption 1.</p>
 				<p class="gallery-widget__details__caption__photo-credit">Photo Credit- Elyssa Cohen</p>
 	    	</div>
 	    </div>
