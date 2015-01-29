bb0cc4a32b68a87ff0357734f3a2392a48cf9e42
EthanGould <ethan.g.907@gmail.com>
2015-01-29 15:35:52 -0500

    Updated GH-Pages with the latest version of this repo that can be used for QA.

--- js/assets/related-articles-main-widget.js
+++ js/assets/related-articles-main-widget.js
@@ -5,7 +5,7 @@
 
 	module.getArticles = function() {
 		$.get('http://devedit.boston.com/eom/SysConfig/WebPortal/BDC/Framework/feeds/placester/getArticles.jsp?mode=full', function(data) {
-			module.allArticles = data['articles'];
+			module.allArticles = data.articles; //['articles'] is better written in dot notation.
 			console.log('All Articles', module.allArticles);
 			module.buildWidget(module.allArticles);
 		});
