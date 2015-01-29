7998b73292a72bff0e944aea6af6a71a4899d4e9
EthanGould <ethan.g.907@gmail.com>
2015-01-29 15:34:21 -0500

    fixes to the follow me map functionality

--- dist/page-content/modules/search-map.php
+++ dist/page-content/modules/search-map.php
@@ -4,17 +4,18 @@
 	    var $sidebar   = $(".sticky-map-wrapper"), 
 	        $window    = $(window),
 	        offset     = $sidebar.offset(),
-	        topPadding = 105;
+	        topPadding = 100;
 
 	    $window.scroll(function() {
-	    	if ($window.scrollTop() > 3200) return;
+	    	var resultsContainerHeight = $('.search-results').height();
+	    	if ($window.scrollTop() > resultsContainerHeight - 345) return;
 	        if ($window.scrollTop() > offset.top) {
 	            $sidebar.stop().animate({
 	                marginTop: $window.scrollTop() - offset.top + topPadding
 	            });
 	        } else {
 	            $sidebar.stop().animate({
-	                marginTop: 0
+	                marginTop: 10
 	            });
 	        }
 	    });
