dbaabf8e167a697fb306d5527352a1a75d2a0742
bv-globe <benjamin.valentine@globe.com>
2015-02-10 19:18:14 -0500

    DM-224: Removing node-modules folder. Adding right rail widget stylesheet + my account html

---
+++ dist/page-content/modules/my-account-right-rail.php
@@ -0,0 +1,68 @@
+
+<div class="right-rail-wrapper">
+	<div id="about-you" class="right-rail-widget">
+		<div class="right-rail-widget__section-header">
+			<h3>About You</h3>
+			<a href="#">Edit Profile</a>
+		</div>
+		<div class="right-rail-widget__container">
+			<ul>
+				<li><span>Name: </span>Ben Valentine</li>
+				<li><span>Email: </span>benjamin.valentine@gmail.com</li>
+				<li><span>Phone: </span>+16175556667</li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="saved-searches" class="rail-rail-widget">
+		<div class="right-rail-widget__section-header">
+			<h3>Saved Searches</h3>
+		</div>
+		<div class="right-rail-widget__container">
+			<p class="right-rail-widget__keywords">garage pool</p>
+			<p class="right-rail-widget__search-type">Purchase: Buy</p>
+			<div class="right-rail-widget__button-list">
+				<ul >
+					<li><a href="">Remove Search</a></li>
+					<li><a href="">Disable Email Notification</a></li>
+					<li><a href="">View Search</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div id="get-in-touch" class="rail-rail-widget">
+		<div class="right-rail-widget__section-header">
+			<h3>Get In Touch</h3>
+		</div>
+		<!-- <div class='contact__container contact__container-basic contact-agent-modal' style='margin-top:58px;'> -->
+			<div class="contact__wrapper">
+				<div class="contact__form">
+					<input type="hidden" name="ip" value="76.119.15.144"/>
+					<input type="hidden" name="user_agent" value="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36"/>
+					<input type="hidden" name="url" value="https://sfgate.placester.net/client-profile/"/>
+					<input type="hidden" name="id" value="">
+					<input type="hidden" name="fullAddress" value="">
+					<input type="hidden" name="email_confirmation" value="">
+					<input type="hidden" name="send_to_email" value="">
+					<input type="hidden" name="cc_value" value="">
+					<input type="hidden" name="bcc_value" value="">
+					<input type="hidden" name="back_on_lc_cancel" value="">
+					<input type="hidden" name="form_submitted" value="0">
+					<input type="hidden" name="custom_link" value="">
+					<input type="hidden" name="custom_link_target" value="_blank">
+					<input type="hidden" name="form_title" value="">
+					<p class='contact__form-required'><span>* </span>Required</p>
+		            <input class='contact__form-name' placeholder='Name'/><span class='contact__required-marker'>*</span>
+		            <input class='contact__form-name' placeholder='Email'/><span class='contact__required-marker'>*</span>
+					<textarea rows="5" id="question" name="question" placeholder="Questions/Comments" required="required" data-message="Don't forget to leave a question or comment."></textarea>
+					<button class='contact__form-submit'>Get in Touch</button>
+				</div>
+			</div>
+		<!-- </div> -->
+	</div>
+	<!-- Get In Touch  -->
+
+</div>
+
+        
