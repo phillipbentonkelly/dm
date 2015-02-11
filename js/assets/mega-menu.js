b47571b3083bdd94d39f0dcfc414770d90fbd147
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-02-11 16:13:11 -0500

    Updated GH-Pages with the latest version of this repo that can be used for QA.

--- js/assets/mega-menu.js
+++ js/assets/mega-menu.js
@@ -11,8 +11,6 @@ var PageNav = {};
 		menuObj: {}
 	};
 
-	var pageSearch = {};
-
 	PageNav = function (){
 	if( ! (this instanceof PageNav ))
 	    return new PageNav();
@@ -20,24 +18,15 @@ var PageNav = {};
 
 	PageNav.prototype = {
 		init: function(){
-
-			pageSearch = $('.page-search');
 			pageNav.$body = $('body');
 			pageNav.$pageNav = $('.page-nav');
 			pageNav.$responsiveIcon = $('.page-nav__mega-menu-responsive-icon');
 			pageNav.$resSearchIcon = $('.page-nav__search-responsive-icon');
-
-			pageNav.$megaMenus = $('.mega-menu');
-			pageNav.$dropDownIcons = $('.page-nav__nav-item__responsive-drop-down-icon');
-			
+			pageNav.menuObj.$findAHome = $('#find-home-menu');
+			pageNav.menuObj.$neighborhoods = $('#neighborhoods-menu');
 			pageNav.menuObj.$logo = $('.page-nav__logo');
 			pageNav.menuObj.$mainNavLinks = $('.page-nav__main-nav-links');
-
-			pageNav.menuObj.$searchSubmit = $('.page-nav__search-submit');
-
-			pageNav.menuObj.$findAHome = $('li#find-home-menu');
-			pageNav.menuObj.$neighborhoods = $('li#neighborhoods-menu');
-			pageNav.menuObj.$myAccount = $('li#myAccount');
+			pageNav.menuObj.$logo = $('.page-nav__logo');
 
 			this.initEventHandlers();
 		}, 
@@ -45,48 +34,20 @@ var PageNav = {};
 			var thisRef = this;
 
 			pageNav.$responsiveIcon.on('click', thisRef.toggleMobileMenu);
-			pageNav.$resSearchIcon.on('click', thisRef.hideMobileMenu);
-
-			pageNav.menuObj.$findAHome.children('a').on('touchstart click', thisRef.toggleMobileMegaMenu);
-			pageNav.menuObj.$neighborhoods.children('a').on('touchstart click', thisRef.toggleMobileMegaMenu);
-			pageNav.menuObj.$myAccount.children('a').on('touchstart click', thisRef.toggleMobileMegaMenu);
-
-			pageNav.menuObj.$searchSubmit.on('click', function(){
-				location.href = 'serp.html';
-			});
+			pageNav.menuObj.$findAHome.on('click', thisRef.toggleMobileMegaMenu);
+			pageNav.menuObj.$neighborhoods.on('click', thisRef.toggleMobileMegaMenu);
 		},
 		toggleMobileMenu: function(e){
 			e.preventDefault();
-			pageNav.menuObj.$mainNavLinks.slideToggle('fast');
+			pageNav.menuObj.$mainNavLinks.toggle();
 		},
-		hideMobileMenu: function(e){
-			pageNav.menuObj.$mainNavLinks.hide('fast');
-		},	
 		toggleMobileMegaMenu: function(e){
-			e.preventDefault();
-			e.stopPropagation();
-
 			var thisRef = this;
-			var thisObj = $(this).parent();
-
-			console.log(thisObj.parent());
+			var thisObj = $(this);
 
 			if(pageNav.$body.width() < 768){
-				//$(this).trigger('focus');
-				var thisToggle = $(this).children('span');
-				var megaMenu = $(thisObj.find('.mega-menu'));
-				var visibleOthers = $('.mega-menu').not(megaMenu).has(':visible');
-				visibleOthers.hide(0, function(){
-					$(this).parent().children('a').children('span').removeClass('minus');
-					$(this).find('.mega-menu__container').hide();
-				});
-				megaMenu.toggle(0, function(){
-					$(this).find('.mega-menu__container').toggle(0, function(){
-						thisToggle.toggleClass('minus');
-					});
-
-				});
-				
+				$(thisObj.find('.mega-menu')).toggle();
+				$(thisObj.find('.mega-menu__container')).toggle();
 			}
 		},
 		updateLogoDim: function(){
@@ -95,6 +56,8 @@ var PageNav = {};
 			var iconObj2 = pageNav.$resSearchIcon;
 			var stage = pageNav.$pageNav;
 			var logoW = stage.width() - (iconObj2.width()+parseInt(iconObj2.css('border-left-width')) + iconObj1.width()+parseInt(iconObj1.css('border-right-width')));
+
+			//console.log("Logo Width: " + pageNav.menuObj.$logo.width());
 
 			if(pageNav.$body.width() < 768){
 				pageNav.menuObj.$logo.width(logoW);
