21041a66451f64924e95f18c27b45afb185fde7e
bv-globe <benjamin.valentine@globe.com>
2015-02-12 16:05:48 -0500

    Updated GH-Pages with the latest version of this repo that can be used for QA.

--- js/assets/newsletter-widget.js
+++ js/assets/newsletter-widget.js
@@ -4,24 +4,18 @@ $(function(){
 	var button = $('.newsletter-widget__signup-button');
 
 	$('.newsletter-widget__signup-button').bind('click', function(e){
-		console.log('hello');
+		e.preventDefault();
+		var base = "http://pages.exacttarget.com/bgcenter/";
+		var mId = "10790730";
+		var pubListId = "19196655";
+		var email = $('.newsletter-widget__signup-input').val();
+		var params = "a=sub&m=" + mId + "&email=" + email + "&l=" + pubListId;
+		var url = base + "?p" + params;
+
+		$.getJSON(url, function(o){
+			alert(o.success);
+		});
+
 	});
-
-	// $('button.newsletter-widget__signup-button').click(function(){
-		// 	e.preventDefault();
-			
-		// 	var base = "http://pages.exacttarget.com/bgcenter/";
-
-		// 	var mId = "10790730";
-		// 	var pubListId = "19196655";
-		// 	var email = $('.newsletter-widget__signup-input').val();
-		// 	var params = "a=sub&mid=" + mId + "&email=" + email + "&l=" + pubListId;
-
-		// 	var url = base + "?p" + Base64.encode(params);
-			
-		// 	$.getJSON(url, function(){
-
-		// });
-	// });
 
 });
