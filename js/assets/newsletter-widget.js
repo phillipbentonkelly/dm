99d9d79c188e39d18a8d793b87f897860b62e0a1
bv-globe <benjamin.valentine@globe.com>
2015-02-16 13:51:41 -0500

    Updated GH-Pages with the latest version of this repo that can be used for QA.

--- js/assets/newsletter-widget.js
+++ js/assets/newsletter-widget.js
@@ -1,18 +1,28 @@
 
 $(function(){
 
-	var button = $('.newsletter-widget__signup-button');
+	var $widget = $('.newsletter-widget__input-wrapper');
+	var $button = $('.newsletter-widget__signup-button');
+	var $input = $('.newsletter-widget__signup-input');
 
 	$('.newsletter-widget__signup-button').bind('click', function(e){
 		e.preventDefault();
-		//var base = "http://pages.exacttarget.com/bgcenter/";
+		var base = "http://pages.exacttarget.com/bgcenter";
 		var mId = '10790730';
 		var pubListId = '19196655';
 		var email = $('.newsletter-widget__signup-input').val();
 
-		var params = 'a=sub&m=' + mId + '&e=' + email + '&l=' + pubListId + '&o=j';
+		var paramObj = {
+			'a' : 'sub',
+			'm' : mId,
+			'l' : pubListId,
+			'e' : email,
+			'o' : 'j'
+		};
 
-		console.log(params);
+		var params = $.param(paramObj);
+
+		//console.log(params);
 
 		$.ajax({
 			url: 'http://pages.exacttarget.com/bgcenter/',
@@ -24,7 +34,15 @@ $(function(){
                 "Access-Control-Allow-Origin": "*"
 			}
 		}).then(function(data){
-			console.log(data.success);
+			if(data.success){
+				var successMsg = '<p class="newsletter-widget__sucess-msg">Success! Thank you</p>';
+				$input.remove();
+				$widget.append(successMsg);
+				
+				$button.text('').style('background: #b48455');
+			}else{
+				console.log(params);
+			}
 		});
 
 	});
